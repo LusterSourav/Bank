e0f4f889a745b56f2784ddc28fda00756f2855c5
LusterSourav <282348889+LusterSourav@users.noreply.github.com>
2026-08-21 16:26:43 +0530

    add license gate at startup

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import connect from './db.js';
 import config from './config.js';
 import{startWatching,stopWatching}from './watcher.js';
 import logger from './logger.js';
+import{check}from './license.js';
 
+check();
 await connect();
 startWatching();
 const srv=app.listen(config.port,()=> logger.info({port:config.port}, 'server listening'));

7cab556e7f4477b82faf25827e21428d55f8fb2c
LusterSourav <282348889+LusterSourav@users.noreply.github.com>
2026-08-21 16:40:23 +0530

    add license gate to Vercel serverless entry

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 // vercel serverless entry. lazy mongo connection on first call
+import{check}from'../src/license.js';
 import app from '../src/app.js';
 import connect from '../src/db.js';
 
-
+check();
 
 let connected=false;
 
